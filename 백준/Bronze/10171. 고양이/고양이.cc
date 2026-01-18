5987563b01a347759e009002fe16c7ff4acd05d2
pm2979 <165562147+pm2979@users.noreply.github.com>
2026-01-18 20:07:24 +0900

    [Bronze V] Title: 고양이, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.cc
@@ -0,0 +1,13 @@
+// 고양이
+#include<iostream>
+using namespace std;
+
+main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	cout << "\\    /\\" << '\n';
+	cout << " )  ( \')" << '\n';
+	cout << "(  /  )" << '\n';
+	cout << " \\(__)|";
+}
